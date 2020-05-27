478a00281d107fdece51c87a630a06bf199a0390
Bosiipo <olabosipo07@gmail.com>
2020-05-27 17:41:11 +0100

    show

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -41,74 +41,74 @@
                         <p class="small my-2">ARTICLE | <span class="text-muted">JAN 22, 2020</span></p> 
                         <h3><a href="/blog/article/hanging-your-work-boot-in-2020" class="text-catalina">Hanging your work boot in 2020?</a></h3>
                     </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/6-ways-to-stay-fit-in-2020" class="">
-                                <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad ls-is-cached lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/newsletter-img-2-300x183.jpg"></a> 
-                                <p class="small my-2">ARTICLE | <span class="text-muted">JAN 22, 2020</span></p> 
-                                <h3><a href="/blog/article/6-ways-to-stay-fit-in-2020" class="text-catalina">6 ways to stay fit in 2020</a></h3>
-                            </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/10-ways-to-make-more-money-in-2020" class="">
-                                <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad ls-is-cached lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/newsletter-img-1-300x166.jpg"></a> 
-                                <p class="small my-2">ARTICLE | <span class="text-muted">JAN 22, 2020</span></p> 
-                                <h3><a href="/blog/article/10-ways-to-make-more-money-in-2020" class="text-catalina">10 ways to make more money in 2020</a></h3>
-                            </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/photo-story-rftf-7-in-pictures" class="">
-                                <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/IMG_9957-300x200.jpg"></a> 
-                                <p class="small my-2">ARTICLE | <span class="text-muted">DEC 9, 2019</span></p> 
-                                <h3><a href="/blog/article/photo-story-rftf-7-in-pictures" class="text-catalina">PHOTO STORY: RFTF 7 in pictures</a></h3>
-                            </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/the-good-the-fun-and-the-winnings" class="">
-                                    <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/runners-image-300x187.jpg">
-                                </a> 
-                                <p class="small my-2">ARTICLE | <span class="text-muted">NOV 21, 2019</span></p> 
-                                <h3><a href="/blog/article/the-good-the-fun-and-the-winnings" class="text-catalina">The Good, The Fun and The Winnings</a></h3>
-                            </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/two-crazy-runners" class="">
-                                    <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/image002-2-300x148.jpg"></a> 
-                                    <p class="small my-2">ARTICLE | <span class="text-muted">OCT 23, 2019</span></p> 
-                                    <h3><a href="/blog/article/two-crazy-runners" class="text-catalina">Two crazy runners</a></h3>
-                            </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/making-an-informed-retirement-decision" class="">
-                                    <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/RETIREMENT-DECISION-300x186.png">
-                                </a> 
-                                <p class="small my-2">ARTICLE | <span class="text-muted">OCT 14, 2019</span></p> 
-                                <h3><a href="/blog/article/making-an-informed-retirement-decision" class="text-catalina">Making an informed retirement decision</a></h3>
-                            </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/can-you-fix-these" class="">
-                                    <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/NEWSLETTER-IMAGE-300x198.jpg">
-                                </a> 
-                                <p class="small my-2">ARTICLE | <span class="text-muted">SEP 23, 2019</span></p> 
-                                <h3><a href="/blog/article/can-you-fix-these" class="text-catalina">Can you fix these?</a></h3>
-                            </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/reaching-financial-freedom" class="">
-                                    <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/thumbnail_NEWSLETTER-REACHING-FINANCIAL-FREEDOM-300x183.jpg">
-                                </a> 
-                                <p class="small my-2">ARTICLE | <span class="text-muted">AUG 14, 2019</span></p> 
-                                <h3><a href="/blog/article/reaching-financial-freedom" class="text-catalina">Reaching Financial Freedom</a></h3>
-                            </div>
-                            <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                <a href="/blog/article/what-is-it-like-living-on-20k-month-find-out-from-this-artisan" class="">
-                                    <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad isLoaded lazyloaded" src="https://www.armpension.com/wp-content/uploads/Explainer-1024x1024-300x300.png">
-                                </a> 
-                                <p class="small my-2">ARTICLE | <span class="text-muted">AUG 14, 2019</span></p> 
-                                <h3>
-                                    <a href="/blog/article/what-is-it-like-living-on-20k-month-find-out-from-this-artisan" class="text-catalina">What is It Like Living on 20k/month? Find out from this Artisan</a>
-                                </h3>
-                                </div>
-                                <div class="col-lg-4 col-md-6 p-4 align-self-center">
-                                    <a href="/blog/article/pfas-encourage-contributors-to-participate-in-the-data-recapturing-exercise" class="">
-                                        <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/cropped-IMG-0892-780x405-300x156.jpg">
-                                    </a> 
-                                    <p class="small my-2">ARTICLE | <span class="text-muted">AUG 1, 2019</span></p> 
-                                    <h3><a href="/blog/article/pfas-encourage-contributors-to-participate-in-the-data-recapturing-exercise" class="text-catalina">PFAs encourage contributors to participate in the Data Recapturing Exercise</a></h3>
-                                </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/6-ways-to-stay-fit-in-2020" class="">
+                        <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad ls-is-cached lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/newsletter-img-2-300x183.jpg"></a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">JAN 22, 2020</span></p> 
+                        <h3><a href="/blog/article/6-ways-to-stay-fit-in-2020" class="text-catalina">6 ways to stay fit in 2020</a></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/10-ways-to-make-more-money-in-2020" class="">
+                        <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad ls-is-cached lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/newsletter-img-1-300x166.jpg"></a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">JAN 22, 2020</span></p> 
+                        <h3><a href="/blog/article/10-ways-to-make-more-money-in-2020" class="text-catalina">10 ways to make more money in 2020</a></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/photo-story-rftf-7-in-pictures" class="">
+                        <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/IMG_9957-300x200.jpg"></a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">DEC 9, 2019</span></p> 
+                        <h3><a href="/blog/article/photo-story-rftf-7-in-pictures" class="text-catalina">PHOTO STORY: RFTF 7 in pictures</a></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/the-good-the-fun-and-the-winnings" class="">
+                            <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/runners-image-300x187.jpg">
+                        </a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">NOV 21, 2019</span></p> 
+                        <h3><a href="/blog/article/the-good-the-fun-and-the-winnings" class="text-catalina">The Good, The Fun and The Winnings</a></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/two-crazy-runners" class="">
+                            <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/image002-2-300x148.jpg"></a> 
+                            <p class="small my-2">ARTICLE | <span class="text-muted">OCT 23, 2019</span></p> 
+                            <h3><a href="/blog/article/two-crazy-runners" class="text-catalina">Two crazy runners</a></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/making-an-informed-retirement-decision" class="">
+                            <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/RETIREMENT-DECISION-300x186.png">
+                        </a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">OCT 14, 2019</span></p> 
+                        <h3><a href="/blog/article/making-an-informed-retirement-decision" class="text-catalina">Making an informed retirement decision</a></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/can-you-fix-these" class="">
+                            <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/NEWSLETTER-IMAGE-300x198.jpg">
+                        </a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">SEP 23, 2019</span></p> 
+                        <h3><a href="/blog/article/can-you-fix-these" class="text-catalina">Can you fix these?</a></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/reaching-financial-freedom" class="">
+                            <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/thumbnail_NEWSLETTER-REACHING-FINANCIAL-FREEDOM-300x183.jpg">
+                        </a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">AUG 14, 2019</span></p> 
+                        <h3><a href="/blog/article/reaching-financial-freedom" class="text-catalina">Reaching Financial Freedom</a></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/what-is-it-like-living-on-20k-month-find-out-from-this-artisan" class="">
+                            <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad isLoaded lazyloaded" src="https://www.armpension.com/wp-content/uploads/Explainer-1024x1024-300x300.png">
+                        </a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">AUG 14, 2019</span></p> 
+                        <h3>
+                            <a href="/blog/article/what-is-it-like-living-on-20k-month-find-out-from-this-artisan" class="text-catalina">What is It Like Living on 20k/month? Find out from this Artisan</a>
+                        </h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 p-4 align-self-center">
+                        <a href="/blog/article/pfas-encourage-contributors-to-participate-in-the-data-recapturing-exercise" class="">
+                            <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad lazyloaded isLoaded" src="https://www.armpension.com/wp-content/uploads/cropped-IMG-0892-780x405-300x156.jpg">
+                        </a> 
+                        <p class="small my-2">ARTICLE | <span class="text-muted">AUG 1, 2019</span></p> 
+                        <h3><a href="/blog/article/pfas-encourage-contributors-to-participate-in-the-data-recapturing-exercise" class="text-catalina">PFAs encourage contributors to participate in the Data Recapturing Exercise</a></h3>
+                    </div>
                                 <div class="col-lg-4 col-md-6 p-4 align-self-center">
                                     <a href="/blog/article/arm-pension-holds-5th-edition-of-investment-research-training" class="">
                                         <img height="200px" alt="" data-srcset="" class="object-cover object-pos-center w-100 rounded-lg lazyLoad isLoaded lazyloaded" src="https://www.armpension.com/wp-content/uploads/DSC_7847-300x199.jpg">
--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,64 @@ Route::get('/blog/article/increase-your-work-productivity-with-these-tips', func
     return view('increase-your-work-productivity-with-these-tips');
 });
 
-/blog/article/hanging-your-work-boot-in-2020
+Route::get('/blog/article/hanging-your-work-boot-in-2020', function () {
+    return view('hanging-your-work-boot-in-2020');
+});
+
+Route::get('/blog/article/6-ways-to-stay-fit-in-2020', function () {
+    return view('6-ways-to-stay-fit-in-2020');
+});
+
+Route::get('/blog/article/10-ways-to-make-more-money-in-2020', function () {
+    return view('10-ways-to-make-more-money-in-2020');
+});
+
+Route::get('/blog/article/photo-story-rftf-7-in-pictures', function () {
+    return view('photo-story-rftf-7-in-pictures');
+});
+
+Route::get('/blog/article/the-good-the-fun-and-the-winnings', function () {
+    return view('the-good-the-fun-and-the-winnings');
+});
+
+Route::get('/blog/article/two-crazy-runners', function () {
+    return view('two-crazy-runners');
+});
+
+Route::get('/blog/article/making-an-informed-retirement-decision', function () {
+    return view('making-an-informed-retirement-decision');
+});
+
+Route::get('/blog/article/can-you-fix-these', function () {
+    return view('can-you-fix-these');
+});
+
+Route::get('/blog/article/reaching-financial-freedom', function () {
+    return view('reaching-financial-freedom');
+});
+
+Route::get('/blog/article/what-is-it-like-living-on-20k-month-find-out-from-this-artisan', function () {
+    return view('what-is-it-like-living-on-20k-month-find-out-from-this-artisan');
+});
+
+Route::get('/blog/article/pfas-encourage-contributors-to-participate-in-the-data-recapturing-exercise', function () {
+    return view('pfas-encourage-contributors-to-participate-in-the-data-recapturing-exercise');
+});
+
+// Route::get('/blog/article/two-crazy-runners', function () {
+//     return view('two-crazy-runners');
+// });
+// Route::get('/blog/article/two-crazy-runners', function () {
+//     return view('two-crazy-runners');
+// });
+// Route::get('/blog/article/two-crazy-runners', function () {
+//     return view('two-crazy-runners');
+// });
+// Route::get('/blog/article/two-crazy-runners', function () {
+//     return view('two-crazy-runners');
+// });
+
+
 
 
 
